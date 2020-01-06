eae3580e05aa74f7579e0e32474c854ade87313e
offerHim <51009968+offerHim@users.noreply.github.com>
2020-01-06 12:01:45 +0800

    router

---
+++ neteasestrictlyselected/src/router/RouterView.tsx
@@ -0,0 +1,40 @@
+import * as React from 'react';
+import { Switch , Redirect , Route } from 'react-router-dom';
+
+interface ItemType{
+    path?: string,
+    component?: any,
+    from?: string,
+    to?: any,
+    children?: ItemType[]
+}
+
+interface PropType{
+    routes: ItemType []
+}
+
+export default (props: PropType)=>{
+    return <div className="warp">
+        <div className="router">
+            <Switch>
+                {
+                    props.routes.map((item: ItemType, index)=>{
+                        if(item.from === '*'){
+                            return <Redirect key={index} to={item.to} />
+                        }else if(item.from){
+                            return <Redirect key={index} exact from={item.from} to={item.to} />
+                        }
+                        return <Route key={index} path={item.path} render={(props)=>{
+                            if(item.children){
+                                return <item.component {...props} routes={item.children} />
+                            }else{
+                                return <item.component {...props} />
+                            }
+                        }}></Route>
+                    })
+                }
+            </Switch>
+        </div>
+        <div className="nav"></div>
+    </div>
+}
